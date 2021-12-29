90171488253a53507db28ae44f4eefee08c29bae
Reg-Sherman <reginaxsherman@gmail.com>
2021-12-29 13:05:41 -0600

    Finished main part, need to work on bonus and figure out the undefined that still prints

--- twelvedays.js
+++ twelvedays.js
@@ -13,3 +13,20 @@
   2: "2 Turtle Doves",
   1: "And a Partridge in a Pear Tree."
 }
+
+function printVerse(i) {
+  if (i >= 1)
+ return `${verses[i]}, ${printVerse(i - 1)}`;
+}
+
+// console.log(printVerse(1))
+
+function printSong(i) {
+  if (i <= 12) {
+    console.log(`On the ${i} day of Christmas, my true love gave to me: ${printVerse(i)}`);
+    printSong(i+1)
+  }
+}
+printSong(1)
+
+
